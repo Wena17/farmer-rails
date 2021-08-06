1e64287b9a2cbf3fccca7c2b5227483d86deac7f
Wena17 <pagayanan27@gmail.com>
2021-08-07 00:44:49 +0800

    Devise added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_25_142214) do
+ActiveRecord::Schema.define(version: 2021_08_06_153638) do
+
+  create_table "jwt_denylist", force: :cascade do |t|
+    t.string "jti", null: false
+    t.datetime "exp", null: false
+    t.index ["jti"], name: "index_jwt_denylist_on_jti"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -20,6 +26,18 @@ ActiveRecord::Schema.define(version: 2021_07_25_142214) do
     t.float "quantity", null: false
     t.decimal "price", precision: 10, scale: 2, null: false
     t.string "unit", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
